818439f8f57b701033d9f18e00e6ee9e113ce500
anaabalbi <anaabalbi@gmail.com>
2022-07-05 10:02:27 -0300

    criando model aluno

---
+++ API/Models/Aluno.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateOnly DatadeNascimento { get; set; }
+        public string Sexo { get; set; }
+        public int TurmaID { get; set; }
+        public int Faltas { get; set; }
+
+    }
+}
