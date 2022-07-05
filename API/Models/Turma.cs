01aef18338e41d105c7cba254161d36470f6f205
anaabalbi <anaabalbi@gmail.com>
2022-07-05 10:03:57 -0300

    criando a model turma

---
+++ API/Models/Turma.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class Turma
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
