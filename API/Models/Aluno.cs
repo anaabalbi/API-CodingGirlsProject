8b96ced574fd9273a8127cb47ab4d2727387e910
anaabalbi <anaabalbi@gmail.com>
2022-07-05 10:20:25 -0300

    modificando o tipo da variável DatadeNascimento

--- API/Models/Aluno.cs
+++ API/Models/Aluno.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
-        public DateOnly DatadeNascimento { get; set; }
+        public DateTime DatadeNascimento { get; set; }
         public string Sexo { get; set; }
         public int TurmaID { get; set; }
         public int Faltas { get; set; }
