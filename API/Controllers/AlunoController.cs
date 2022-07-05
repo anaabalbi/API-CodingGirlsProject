17c3e16a7f0f7905df46698c1d611c14ad8f2957
anaabalbi <anaabalbi@gmail.com>
2022-07-05 10:23:11 -0300

    criando a AlunoController

---
+++ API/Controllers/AlunoController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Context;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        private readonly APIContext _context;
+
+        public AlunoController(APIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Aluno
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Aluno>>> GetAluno()
+        {
+          if (_context.Aluno == null)
+          {
+              return NotFound();
+          }
+            return await _context.Aluno.ToListAsync();
+        }
+
+        // GET: api/Aluno/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+          if (_context.Aluno == null)
+          {
+              return NotFound();
+          }
+            var aluno = await _context.Aluno.FindAsync(id);
+
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return aluno;
+        }
+
+        // PUT: api/Aluno/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAluno(int id, Aluno aluno)
+        {
+            if (id != aluno.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(aluno).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlunoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Aluno
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Aluno>> PostAluno(Aluno aluno)
+        {
+          if (_context.Aluno == null)
+          {
+              return Problem("Entity set 'APIContext.Aluno'  is null.");
+          }
+            _context.Aluno.Add(aluno);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAluno", new { id = aluno.Id }, aluno);
+        }
+
+        // DELETE: api/Aluno/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAluno(int id)
+        {
+            if (_context.Aluno == null)
+            {
+                return NotFound();
+            }
+            var aluno = await _context.Aluno.FindAsync(id);
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            _context.Aluno.Remove(aluno);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AlunoExists(int id)
+        {
+            return (_context.Aluno?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
