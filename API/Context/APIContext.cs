31035c678c4811e397da155a2f2758ff13e143ab
anaabalbi <anaabalbi@gmail.com>
2022-07-05 10:08:58 -0300

    criando o context

---
+++ API/Context/APIContext.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Context
+{
+    public class APIContext :DbContext
+
+    {
+        public APIContext(DbContextOptions<APIContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Aluno> Aluno { get; set; }
+        public DbSet<Turma> Turma { get; set; }
+
+        
+    }
+}
