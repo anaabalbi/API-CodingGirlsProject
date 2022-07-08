f0359a7b06b0c18ef690313754fa974843cbf34e
anaabalbi <anaabalbi@gmail.com>
2022-07-08 14:05:46 -0300

    Criando métodos na model Aluno

--- API/Models/Turma.cs
+++ API/Models/Turma.cs
@@ -7,5 +7,33 @@
         public bool Ativo { get; set; }
 
 
+        public List<Turma> TurmasAtivas(List<Turma> turmas)
+        {
+            List<Turma> turmasList = new();
+            foreach(Turma turma in turmas)
+            {
+                if (turma.Ativo)
+                {
+                    turmasList.Add(new Turma { Id = turma.Id, Nome = turma.Nome, Ativo = turma.Ativo });
+                }
+            }
+            return turmasList;
+        }
+
+        public bool TemAlunos(int id, List<Aluno> aluno)
+        {
+            foreach(Aluno alunos in aluno)
+            {
+                if (alunos.TurmaID == id)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
     }
+
+
+
 }
